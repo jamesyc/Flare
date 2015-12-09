d8db64ff005324384e705d02fe88b7d852f178ef
ADanilychevJr <danilychev@berkeley.edu>
2015-12-09 00:58:54 -0800

    Finished gesture tutorial

--- Flare/mobile/src/main/java/com/cs160/group14/flare/TutorialActivity.java
+++ Flare/mobile/src/main/java/com/cs160/group14/flare/TutorialActivity.java
@@ -1,6 +1,8 @@
 package com.cs160.group14.flare;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,6 +15,8 @@ public class TutorialActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tutorial_view);
+        getActionBar().setTitle("Flare Tutorial");
+        getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#eb9a60")));
     }
 
     public void onExitClick(View v){
